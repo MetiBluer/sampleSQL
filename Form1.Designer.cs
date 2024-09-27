e75868ec3261be079ce9a032b28e8e879fe0cb06
MetiBluer <133645933+MetiBluer@users.noreply.github.com>
2024-09-27 15:32:54 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SampleMySql
+{
+    partial class frmSqL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSql = new System.Windows.Forms.DataGridView();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSql)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSql
+            // 
+            this.dgvSql.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSql.Location = new System.Drawing.Point(24, 12);
+            this.dgvSql.Name = "dgvSql";
+            this.dgvSql.Size = new System.Drawing.Size(539, 352);
+            this.dgvSql.TabIndex = 0;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(95, 370);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.TabIndex = 1;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(218, 370);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(396, 373);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // frmSqL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 458);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.dgvSql);
+            this.Name = "frmSqL";
+            this.Text = "My sql ";
+            this.Load += new System.EventHandler(this.frmSqL_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSql)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSql;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SampleMySql
+{
+    public partial class frmSqL : Form
+    {   //connection string
+        string connStr = "server=localhost; database=dbsample; uid=root; pwd=uslt; port=3306";
+        MySqlConnection conn;
+        DataTable dt = new DataTable();
+
+
+        public frmSqL()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSqL_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            conn = new MySqlConnection(connStr);
+            string query = "select * from student where fname='"+txtSearch.Text+"'; ";
+            conn.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(query,conn);
+            adapter.Fill(dt);
+            dgvSql.DataSource = dt;
+        }
+    }
+}
